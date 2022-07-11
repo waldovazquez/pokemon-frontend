8b8b66355cffbcb8cc149f03e324a9d4da58a08f
Waldo Vazquez <waldovazquezdev@gmail.com>
2022-07-10 21:56:42 -0300

    adding search in pokemon

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -20,10 +20,13 @@ import Screen from '../../components/Screen';
 import Loading from '../../components/Loading';
 import Card from '../../components/Card';
 import Toast from '../../components/Toast';
+import Input from '../../components/Input';
 
 import {
   API_URL,
 } from '../../utils/constants';
+
+import Search from '../../assets/search.png';
 
 import styles from './home.module.css';
 
@@ -34,6 +37,7 @@ function Home() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [pokemons, setPokemons] = useState([]);
   const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
   const [loading, setLoading] = useState(false);
   const [totalPages, setTotalPages] = useState(1);
   const [alert, setAlert] = useState(null);
@@ -41,7 +45,16 @@ function Home() {
   async function getPokemons() {
     try {
       setLoading(true);
-      const response = await axios.get(`${API_URL}/pokemon/getallpokemons?page=${page}`);
+      const filters = {};
+      if (query) {
+        filters.query = query;
+      }
+      filters.page = page;
+
+      const response = await axios.get(`${API_URL}/pokemon/getallpokemons`, {
+        params: filters,
+      });
+
       if (response && response.data) {
         setTotalPages(response.data.totalPages);
         setPokemons(response.data.docs);
@@ -85,6 +98,7 @@ function Home() {
     getPokemons();
   }, [
     page,
+    query,
   ]);
 
   useEffect(() => {
@@ -96,7 +110,9 @@ function Home() {
     } else {
       handlePage(1);
     }
-  }, [searchParams]);
+  }, [
+    searchParams,
+  ]);
 
   return (
     <Screen>
@@ -106,23 +122,39 @@ function Home() {
         }}
         className={styles.container}
       >
-        {
-          pokemons && pokemons.map((item) => (
-            <div
-              key={item._id}
-              style={{
-                margin: '6px',
-              }}
-            >
-              <Card
-                image={item.image}
-                title={item.name}
-                id={item._id}
-                onClick={() => addToFavorites(item._id)}
-              />
-            </div>
-          ))
-        }
+        <div
+          className={styles.container__search}
+        >
+          <Input
+            placeholder="Search..."
+            activeIcon
+            icon={Search}
+            alt="search"
+            className={styles.component__input}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </div>
+        <div
+          className={styles.container__cards}
+        >
+          {
+            pokemons && pokemons.map((item) => (
+              <div
+                key={item._id}
+                style={{
+                  margin: '6px',
+                }}
+              >
+                <Card
+                  image={item.image}
+                  title={item.name}
+                  id={item._id}
+                  onClick={() => addToFavorites(item._id)}
+                />
+              </div>
+            ))
+          }
+        </div>
       </div>
       <div style={{
         height: '50px',
